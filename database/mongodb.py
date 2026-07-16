cd248e9ed07b19123af981dd37b62480c595aaa3
sonuPanwar123 <150221272+sonuPanwar123@users.noreply.github.com>
2026-07-16 16:08:19 +0530

    Update mongodb.py

--- database/mongodb.py
+++ database/mongodb.py
@@ -4,7 +4,7 @@ try:
     MONGO_URI = "mongodb+srv://sp6345607_db_user:CJWMNRaLVuXaTJzM@cluster0.ckknkxv.mongodb.net/?appName=Cluster0"
     client = MongoClient(MONGO_URI)
     client.admin.command("ping")
-    db = client["ssus"]
+    db = client["ssus123"]
     students_collection = db["students"]
     marks_collection = db["marks"]
     attendance_collection = db["attendance"]
